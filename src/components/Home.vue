219f95286f8dbeb7b5d0b01265b22a0be71ed37a
xlegoleg <xxxlegoleg@gmail.com>
2020-07-29 22:25:10 +0300

    add modal, handlers, etc.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,38 +12,74 @@
                     sm="6"
             >
                 <v-card
+                        :ripple="{center: true, class: 'purple--text text--darken-4'}"
                         class="home-card d-flex flex-column align-center justify-center"
+                        @click="clickHandler($event,    item.callback)"
                 >
                     <div
                             class="home-card__round d-flex align-center justify-center"
                     >
                         <v-icon>{{item.icon}}</v-icon>
                     </div>
-                    <h2>{{item.title}}</h2>
+                    <h3>{{item.title}}</h3>
                 </v-card>
             </v-col>
         </v-row>
+        <ChatModal
+                :modal="modal"
+                :cChat="cChat"
+                :jChat="jChat"
+                v-on:update:modal="modal = $event"
+        >
+
+
+        </ChatModal>
     </v-container>
 </template>
 
 <script>
 
-// import Chat from '@/components/chat/Chat'
+import ChatModal from '@/components/common/ChatModal'
 
 export default {
 
     name: 'Home',
 
     components: {
-        // Chat
+        ChatModal
     },
 
     data() {
         return {
+            modal: false,
+            cChat: false,
+            jChat: false,
             items: [
-                {title: 'Create new chat', icon: 'mdi-chat-plus-outline'},
-                {title: 'Join existing chat', icon: 'mdi-chat-processing-outline'}
+                {title: 'Create new chat', icon: 'mdi-chat-plus-outline',callback: this.createChat},
+                {title: 'Join existing chat', icon: 'mdi-chat-processing-outline',callback: this.joinChat}
             ]
+        }
+    },
+
+    methods: {
+        /**
+         * Card click handler
+         * @param e {Object}
+         * @param callback {function}
+         */
+        clickHandler(e, callback) {
+            callback();
+            this.modal = true;
+        },
+
+        createChat() {
+            this.cChat = true;
+            this.jChat = false;
+        },
+
+        joinChat() {
+            this.cChat = false;
+            this.jChat = true;
         }
     }
 
@@ -55,18 +91,25 @@ export default {
     .home{
         min-height: 100%;
         &-card{
-            height: 230px;
+            height: 180px;
+            transition: all 0.2s ease-in;
+            opacity: 0.6;
             &__round{
-                background-color: $main-grey;
-                width: 180px;
-                height: 180px;
+                background-color: $main-color;
+                width: 130px;
+                height: 130px;
                 border-radius: 50% !important;
                 i{
-                    font-size: 70px !important;
+                    font-size: 50px !important;
+                    color: #ffffff !important;
                 }
             }
-            h2{
-                color: $main-light;
+            h3{
+                color: $main-color;
+            }
+            &:hover{
+                opacity: 1;
+                cursor: pointer;
             }
         }
     }
---
+++ src/components/common/ChatModal.vue
@@ -0,0 +1,91 @@
+<template>
+    <v-dialog
+            v-model="modalOpen"
+            max-width="400"
+    >
+     <v-card>
+         <v-card-title>{{title}}</v-card-title>
+         <v-card-text class="pb-1">
+             <v-text-field
+                     v-model="chatName"
+                     label="Enter chat name"
+                     single-line
+                     outlined
+                     hide-details
+                     :color="mainColor"
+             >
+
+             </v-text-field>
+         </v-card-text>
+         <v-card-actions class="text-center px-6 pb-4">
+             <v-btn
+                     v-if="cChat"
+                     @click="createHandler"
+                     type="submit"
+                     block
+                     color="green"
+                     class="white--text"
+             >
+                 Create chat
+             </v-btn>
+             <v-btn
+                     v-if="jChat"
+                     @click="joinHandler"
+                     type="submit"
+                     block
+                     color="green"
+                     class="white--text"
+             >
+                 Join chat
+             </v-btn>
+         </v-card-actions>
+     </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    import {mapState,mapActions} from 'vuex';
+
+    export default {
+        name: 'ChatModal',
+
+        props: ['modal', 'cChat', 'jChat'],
+
+        data() {
+            return {
+                title: 'Enter chat name:',
+                chatName: '',
+                open: false
+            }
+        },
+
+        mounted() {
+            this.open = this.modal;
+        },
+
+        computed: {
+            ...mapState({
+                mainColor: state => state.mainColor
+            }),
+            ...mapActions({
+
+            }),
+
+            modalOpen: {
+                get() {return this.modal},
+                set (value) { this.$emit('update:modal', value) },
+            }
+        },
+
+        methods: {
+            createHandler() {
+
+            },
+
+            joinHandler() {
+
+            }
+        }
+    }
+
+</script>
